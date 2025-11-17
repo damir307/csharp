29a37f3dd9644f3b86107b7efaab50bbb2c653b9
damir307 <darmenovdanil@gmail.com>
2025-11-17 20:39:44 +0500

    Добавлены все задания

--- ConsoleApp4/ConsoleApp4/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿Console.WriteLine("{0,-10} {1,10}", "Name", "Count");
-
-Console.WriteLine("{0,-10} {1,10:N0}", "Apples", 12345);
-Console.WriteLine("{0,-10} {1,10:N0}", "Oranges", 567);
-Console.WriteLine("{0,-10} {1,10:N0}", "Milk", 89);
