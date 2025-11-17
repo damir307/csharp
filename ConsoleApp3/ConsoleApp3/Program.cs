62b1387525a28a24d2ad66ed2a07e56dacd68893
damir307 <darmenovdanil@gmail.com>
2025-11-17 20:24:52 +0500

    Initial commit with all tasks

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Enter name: ");
+string name = Console.ReadLine();
+
+Console.Write("Enter age: ");
+int age = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Hello {name}, you look good for {age}.");
---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("{0,-10} {1,10}", "Name", "Count");
+
+Console.WriteLine("{0,-10} {1,10:N0}", "Apples", 12345);
+Console.WriteLine("{0,-10} {1,10:N0}", "Oranges", 567);
+Console.WriteLine("{0,-10} {1,10:N0}", "Milk", 89);
