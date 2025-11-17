9e6f73c84d681ddd91f0ab19ef64c65b324b2e37
damir307 <darmenovdanil@gmail.com>
2025-11-17 20:43:17 +0500

    Добавлены все задания

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,13 @@
+﻿string? nickname = null;
+
+int GetNicknameLength(string? s) => s?.Length ?? 0;
+
+Console.Write("Enter nickname (empty allowed): ");
+string input = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(input))
+    nickname = null;
+else
+    nickname = input;
+
+Console.WriteLine($"Length: {GetNicknameLength(nickname)}");
---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Enter something: ");
+string s = Console.ReadLine();
+
+if (int.TryParse(s, out int i))
+{
+    Console.WriteLine("This is int");
+}
+else if (double.TryParse(s, out double d))
+{
+    Console.WriteLine("This is double");
+}
+else if (bool.TryParse(s, out bool b))
+{
+    Console.WriteLine("This is bool");
+}
+else
+{
+    Console.WriteLine("Unknown type (string)");
+}
